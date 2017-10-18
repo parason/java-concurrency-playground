b3e4abc8ee9798d9fd957aa60a3a7731145306b8
Ruslan Kvashchanka <r.kvashchanka@gmail.com>
2017-10-18 22:35:19 +0200

    started to implement

--- fork-join-sort/src/main/java/org/jcp/forkjoin/sort/ParallelMergeSort.java
+++ fork-join-sort/src/main/java/org/jcp/forkjoin/sort/ParallelMergeSort.java
@@ -1,6 +1,10 @@
 package org.jcp.forkjoin.sort;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 /**
  * https://en.wikipedia.org/wiki/Merge_sort#Parallel_merge_sort
@@ -8,28 +12,67 @@ import java.util.List;
  */
 public class ParallelMergeSort {
 
-    public static <T> List<T> sort(final List<T> list) {
+    public static <T extends Comparable<? super T>> List<T> sort(final List<T> list) {
+
         if (list == null || list.size() < 2) {
             return list;
         }
 
-        return new ForkJoinMergeSort<>(list).sort();
+        ForkJoinPool forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+
+        forkJoinPool.invoke(new ForkJoinMergeSort<>(list, 0, list.size()));
+
+        return list;
     }
 
 
-    private static class ForkJoinMergeSort<T> {
+    private static class ForkJoinMergeSort<T extends Comparable<? super T>> extends RecursiveAction {
 
         private final List<T> list;
+        private final int high;
+        private final int low;
 
-        ForkJoinMergeSort(final List<T> list) {
+        ForkJoinMergeSort(final List<T> list, final int low, final int high) {
             this.list = list;
+            this.low = low;
+            this.high = high;
         }
 
-        List<T> sort() {
-
-            throw new UnsupportedOperationException("Not implemented yet...");
+        void merge(final int middle) {
+            final Comparator<T> comparator = Comparator.naturalOrder();
+            final List<T> buffer = new ArrayList<>(list.subList(low, high));
+            int i = low;
+            int j = middle + 1;
+            int k = high;
+            while (i <= middle && j <= high) {
+                if (comparator.compare(buffer.get(i), buffer.get(j)) <= 0) {
+                    list.set(k, buffer.get(i));
+                    i++;
+                } else {
+                    list.set(k, buffer.get(j));
+                    j++;
+                }
+                k++;
+            }
+            while (i <= middle) {
+                list.set(k, buffer.get(i));
+                k++;
+                i++;
+            }
         }
 
 
+        @Override
+        protected void compute() {
+            if (high - low <= 1) {
+                return;
+            }
+
+            int middle = (high + low) / 2;
+
+            invokeAll(new ForkJoinMergeSort<>(list, low, middle), new ForkJoinMergeSort<>(list, middle + 1, high));
+
+            merge(middle);
+        }
     }
 }
--- fork-join-sort/src/test/java/org/jcp/forkjoin/sort/ParallelMergeSortTest.java
+++ fork-join-sort/src/test/java/org/jcp/forkjoin/sort/ParallelMergeSortTest.java
@@ -1,12 +1,33 @@
 package org.jcp.forkjoin.sort;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
 public class ParallelMergeSortTest {
+
     @Test
-    public void test1() throws Exception {
+    public void test() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(ThreadLocalRandom.current().nextInt());
+        }
+
+        System.out.println("In");
+
+        list.forEach(System.out::println);
+
+        list = ParallelMergeSort.sort(list);
+
+        LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(10));
+
+        System.out.println("Out");
+
+        list.forEach(System.out::println);
     }
 
 }
