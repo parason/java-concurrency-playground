f7c73a0e722af547337b3445c2c1aec06ae59982
Ruslan Kvashchanka <r.kvashchanka@gmail.com>
2017-10-19 16:02:22 +0200

    implemented mergesort, added some tests and javadocs

--- fork-join-sort/src/main/java/org/jcp/forkjoin/sort/ParallelMergeSort.java
+++ fork-join-sort/src/main/java/org/jcp/forkjoin/sort/ParallelMergeSort.java
@@ -1,6 +1,7 @@
 package org.jcp.forkjoin.sort;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
@@ -12,67 +13,130 @@ import java.util.concurrent.RecursiveAction;
  */
 public class ParallelMergeSort {
 
+    /**
+     * A short-cut for {@link #sort(List, Comparator, int)} below with the {@link Comparator#naturalOrder()} as the comparator
+     * and Runtime.getRuntime().availableProcessors() parallelism level
+     *
+     * @param list to be sorted
+     * @param <T>  type of elements in the list
+     * @return sorted list
+     */
     public static <T extends Comparable<? super T>> List<T> sort(final List<T> list) {
+        return sort(list, Comparator.naturalOrder(), Runtime.getRuntime().availableProcessors());
+    }
+
+    /**
+     * A short-cut for {@link #sort(List, Comparator, int)} below with the Runtime.getRuntime().availableProcessors()
+     * parallelism level
+     *
+     * @param list       to be sorted
+     * @param comparator to be used while sorting to determine the element order
+     * @param <T>        type of elements in the list
+     * @return sorted list
+     */
+    public static <T extends Comparable<? super T>> List<T> sort(final List<T> list, final Comparator<T> comparator) {
+        return sort(list, comparator, Runtime.getRuntime().availableProcessors());
+    }
+
+    /**
+     * A short-cut for {@link #sort(List, Comparator, int)} below with the {@link Comparator#naturalOrder()} as the comparator
+     *
+     * @param list        to be sorted
+     * @param parallelism level of the Fork-Join Framework
+     * @param <T>         type of elements in the list
+     * @return sorted list
+     */
+    public static <T extends Comparable<? super T>> List<T> sort(final List<T> list, final int parallelism) {
+        return sort(list, Comparator.naturalOrder(), parallelism);
+    }
+
+    /**
+     * Does the soring trick
+     *
+     * @param list        to be sorted
+     * @param comparator  to be used while sorting to determine the element order
+     * @param parallelism level of the Fork-Join Framework
+     * @param <T>         type of elements in the list
+     * @return sorted list
+     */
+    public static <T extends Comparable<? super T>> List<T> sort(final List<T> list,
+            final Comparator<T> comparator,
+            final int parallelism) {
 
         if (list == null || list.size() < 2) {
             return list;
         }
 
-        ForkJoinPool forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+        ForkJoinPool forkJoinPool = new ForkJoinPool(parallelism);
 
-        forkJoinPool.invoke(new ForkJoinMergeSort<>(list, 0, list.size()));
+        forkJoinPool.invoke(new ForkJoinMergeSort<>(list, comparator, 0, list.size()));
 
         return list;
     }
 
 
+    /**
+     * A {@link RecursiveAction} implementation that does the trick
+     *
+     * @param <T> type of elements to be sorted
+     */
     private static class ForkJoinMergeSort<T extends Comparable<? super T>> extends RecursiveAction {
 
-        private final List<T> list;
-        private final int high;
-        private final int low;
+        private final List<T>       list;
+        private final Comparator<T> comparator;
+        private final int           high;
+        private final int           low;
 
-        ForkJoinMergeSort(final List<T> list, final int low, final int high) {
+
+        /**
+         * Creates a new instance with
+         *
+         * @param list       to be sorted
+         * @param comparator to be used for element order determination
+         * @param low        lower list bound
+         * @param high       upper list bound
+         */
+        ForkJoinMergeSort(final List<T> list, final Comparator<T> comparator, final int low, final int high) {
             this.list = list;
+            this.comparator = comparator;
             this.low = low;
             this.high = high;
         }
 
-        void merge(final int middle) {
-            final Comparator<T> comparator = Comparator.naturalOrder();
-            final List<T> buffer = new ArrayList<>(list.subList(low, high));
-            int i = low;
-            int j = middle + 1;
-            int k = high;
-            while (i <= middle && j <= high) {
-                if (comparator.compare(buffer.get(i), buffer.get(j)) <= 0) {
-                    list.set(k, buffer.get(i));
-                    i++;
+        /**
+         * "Sorts" everything back together
+         */
+        void merge() {
+            final List<T> buffer          = Collections.unmodifiableList(new ArrayList<>(list.subList(low, high)));
+            final int     bufferLastIndex = buffer.size() - 1;
+            final int     bufferMiddle    = (high - low) / 2;
+            // re-arrange items in the list according to buffer
+            for (int listIndex = low, bufferLeftIndex = 0, bufferRightIndex = bufferMiddle; listIndex < high; listIndex++) {
+                if (bufferRightIndex > bufferLastIndex ||
+                        (bufferLeftIndex < bufferMiddle &&
+                                comparator.compare(buffer.get(bufferLeftIndex), buffer.get(bufferRightIndex)) <= 0)) {
+                    list.set(listIndex, buffer.get(bufferLeftIndex++));
                 } else {
-                    list.set(k, buffer.get(j));
-                    j++;
+                    list.set(listIndex, buffer.get(bufferRightIndex++));
                 }
-                k++;
-            }
-            while (i <= middle) {
-                list.set(k, buffer.get(i));
-                k++;
-                i++;
             }
         }
 
-
+        /**
+         * Splits the list until single element and merges everything back
+         */
         @Override
         protected void compute() {
+            // consider as sorted
             if (high - low <= 1) {
                 return;
             }
-
-            int middle = (high + low) / 2;
-
-            invokeAll(new ForkJoinMergeSort<>(list, low, middle), new ForkJoinMergeSort<>(list, middle + 1, high));
-
-            merge(middle);
+            // get the middle index
+            int middle = low + ((high - low) / 2);
+            // recursive split
+            invokeAll(new ForkJoinMergeSort<>(list, comparator, low, middle), new ForkJoinMergeSort<>(list, comparator, middle, high));
+            // put everything back together
+            merge();
         }
     }
 }
--- fork-join-sort/src/test/java/org/jcp/forkjoin/sort/ParallelMergeSortTest.java
+++ fork-join-sort/src/test/java/org/jcp/forkjoin/sort/ParallelMergeSortTest.java
@@ -1,33 +1,44 @@
 package org.jcp.forkjoin.sort;
 
-import org.junit.Test;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.LockSupport;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ParallelMergeSortTest {
 
     @Test
-    public void test() throws Exception {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+    public void testBasic() throws Exception {
+        final List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1_000_000; i++) {
             list.add(ThreadLocalRandom.current().nextInt());
         }
 
-        System.out.println("In");
+        final List<Integer> expected = new ArrayList<>(list);
+        Collections.sort(expected);
 
-        list.forEach(System.out::println);
+        ParallelMergeSort.sort(list);
 
+        Assert.assertEquals(expected, list);
+    }
+
+    @Test
+    public void testOneElement() {
+        final List<Integer> list = Collections.singletonList(1);
+        ParallelMergeSort.sort(list);
+        Assert.assertEquals(list, Collections.singletonList(1));
+    }
+
+    @Test
+    public void testTwoElements() {
+        List<Integer>       list     = Arrays.asList(2, 1);
+        final List<Integer> expected = Arrays.asList(1, 2);
         list = ParallelMergeSort.sort(list);
-
-        LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(10));
-
-        System.out.println("Out");
-
-        list.forEach(System.out::println);
+        Assert.assertEquals(list, expected);
     }
 
 }
